c8ae8316b1543c8ff4e5f61371e7569a4b3040fd
cuijiaxun <cuijiaxun_sjtu@163.com>
2021-11-22 13:47:02 -0600

    debug build tree

--- build_tree.py
+++ build_tree.py
@@ -13,7 +13,7 @@ def build_tree(items=None, dist_lookup=None, b=0, L=0):
     for item in index_name:
         C_lower.append(Node(item[0], item[1], L))
     for i in range(L-1):
-        l=L-i
+        l=L-i-1
         C_higher = []
         while len(visited_lower)>0:
             idx = random.choice(visited_lower)
@@ -21,31 +21,36 @@ def build_tree(items=None, dist_lookup=None, b=0, L=0):
             visited_lower.remove(idx)
             for i in range(len(C_lower)):
                 c = C_lower[i]
-                if c.name!=new_node.name and dist_lookup[c.name][new_node.name] < (1-b)*b**l/(1+b) and i in visited_lower:
-                    new_Node.add_child(c)
-                    c.add_parent(new_Node)
+                threshold = (1-b)*b**l/(1+b)
+                if c.name!=new_node.name and dist_lookup[c.name][new_node.name] <= b**(l-1) and i in visited_lower:
+                    new_node.add_child(c)
+                    c.set_parent(new_node)
                     visited_lower.remove(i)
             C_higher.append(new_node)
         C_lower=C_higher
         visited_lower=list(np.arange(len(C_lower)))
-    
+        print("Level {}".format(l))
+        for c in C_higher: print(c.name)
     return C_higher 
 
-def build_dist_lookup(data):
+def build_dist_lookup(data, normalization=True):
     """
     This function builds a distance look up table for two points
     It is undirected 
     In the form of [node1][node2][distance]
     """
     dists = {}
+    max_dist = 0
+    for d in data:
+        max_dist = max(max_dist, float(d[2]))
+
     for d in data:
         if d[0] not in dists.keys():
             dists[d[0]] = {}
         if d[1] not in dists.keys():
             dists[d[1]] = {}
-        dists[d[1]][d[0]] = float(d[2])
-        dists[d[0]][d[1]] = float(d[2])
-
+        dists[d[1]][d[0]] = float(d[2])/max_dist
+        dists[d[0]][d[1]] = float(d[2])/max_dist
     return dists
 
 if __name__ == '__main__':
@@ -54,4 +59,4 @@ if __name__ == '__main__':
     data = data.values.tolist()[1:] 
     dist_lookup = build_dist_lookup(data)
     items = sorted(list(dist_lookup.keys()))
-    build_tree(items, dist_lookup, b=0.6, L=3)
+    build_tree(items, dist_lookup, b=0.6, L=4)
